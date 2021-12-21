51689e2d5e13b920fff7756217fcd751a88f7243
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-12-21 07:53:23 +0300

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DivComponent } from './div/div.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DivComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DivComponent } from './div.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('DivComponent', () => {
-  let component: DivComponent;
-  let fixture: ComponentFixture<DivComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DivComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DivComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
